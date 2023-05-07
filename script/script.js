5ceb45c900406bce08bc9235da30c9a52d83470c
Devin Winando <devin.winando13@gmail.com>
2023-05-07 16:29:33 +0700

    fix: variable name

--- script/script.js
+++ script/script.js
@@ -31,7 +31,7 @@ document.addEventListener("change", async (e) => {
 document.addEventListener("change", (e) => {
   if (e.target.classList.contains("kota")) {
     searchBtn.disabled = false;
-    const idKot = e.target.value;
+    const idKota = e.target.value;
     searchBtn.dataset.idkota = idKota;
   }
 });
